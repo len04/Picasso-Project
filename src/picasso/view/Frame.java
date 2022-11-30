897c68d57bf17e8b7d84e9d07bf33065c4a2d217
FekryMostafa <mostafaf25@mail.wlu.edu>
2022-11-30 13:18:12 -0500

    Figuring out GUI input

--- src/picasso/view/Frame.java
+++ src/picasso/view/Frame.java
@@ -1,9 +1,12 @@
 package picasso.view;
 
 import java.awt.*;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 import picasso.model.Pixmap;
+import picasso.util.NamedCommand;
 import picasso.util.ThreadedCommand;
 import picasso.view.commands.*;
 
@@ -21,20 +24,26 @@ public class Frame extends JFrame {
 		// create GUI components
 		Canvas canvas = new Canvas(this);
 		canvas.setSize(size);
+		
 		JLabel inputLabel = new JLabel ("Enter expression: ");
 		JTextField inputField = new JTextField(10);
-
+		
 		// add commands to test here
 		ButtonPanel commands = new ButtonPanel(canvas);
 		commands.add("Open", new Reader());
 		commands.add("Save", new Writer());
+		commands.add("Evaluate", new ThreadedCommand<Pixmap>(canvas, new Evaluator()));	
+		
 		commands.add(inputLabel);
 		commands.add(inputField);	
-		commands.add("Evaluate", new ThreadedCommand<Pixmap>(canvas, new Evaluator()));		
+		commands.add("Submit", new NamedCommand(canvas, new Evaluator()));			
+		
+		// inputField.addActionListener((ActionListener) this);
 
 		// add our container to Frame and show it
 		getContentPane().add(canvas, BorderLayout.CENTER);
 		getContentPane().add(commands, BorderLayout.NORTH);
+		
 		pack();
 	}
 }
