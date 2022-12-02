8b0a0e2956324f2dd7a86bd965180b70eb4b7457
stockm23 <97920145+stockm23@users.noreply.github.com>
2022-12-01 19:48:06 -0500

    added tan

--- src/tests/ParseExpressionTreeTests.java
+++ src/tests/ParseExpressionTreeTests.java
@@ -72,4 +72,13 @@ public class ParseExpressionTreeTests {
 		assertEquals(new Floor(new Addition(new X(), new Y())), e);
 	}
 
+	@Test
+	public void tangentFunctionTests() {
+		ExpressionTreeNode e = parser.makeExpression("tan( x )");
+		assertEquals(new Tan(new X()), e);
+
+		e = parser.makeExpression("tan( x + y )");
+		assertEquals(new Tan(new Addition(new X(), new Y())), e);
+	}
+	
 }
