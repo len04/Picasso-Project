75863ea2e5aaa2f1cc6c6e5c3e523492c77f547f
stockm23 <97920145+stockm23@users.noreply.github.com>
2022-12-01 19:41:22 -0500

    added tan

---
+++ src/picasso/parser/tokens/functions/TanToken.java
@@ -0,0 +1,13 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the tangent function token
+ * 
+ */
+public class TanToken extends FunctionToken {
+
+	public TanToken() {
+		super("Tangent Function Token");
+	}
+
+}
