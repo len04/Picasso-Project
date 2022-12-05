ef8377ce689eb3aefec801ac7172da262ece9238
petrailic <ilicp24@mail.wlu.edu>
2022-12-05 12:39:15 -0500

    implemented wrap function

---
+++ src/picasso/parser/tokens/functions/WrapToken.java
@@ -0,0 +1,14 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the wrap function token
+ * 
+ * @author Petra Ilic
+ */
+public class WrapToken extends FunctionToken {
+
+	public WrapToken() {
+		super("Wrap Function Token");
+	}
+
+}
