3f44b18efceaa2f5f80e6ec8581f99664a52abbd
FekryMostafa <mostafaf25@mail.wlu.edu>
2022-12-08 01:31:57 -0500

    Assignment expression half-working

---
+++ src/picasso/parser/AssignmentAnalyzer.java
@@ -0,0 +1,29 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Assignment;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing the Assignment operation.
+ * 
+ * @author Fekry Mostafa
+ * 
+ */
+public class AssignmentAnalyzer implements SemanticAnalyzerInterface {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); // Remove the times token
+		// the parameters are the next tokens on the stack.
+		ExpressionTreeNode param1 = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		ExpressionTreeNode param2 = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+				
+		return new Assignment(param2, param1);
+	}
+
+}
