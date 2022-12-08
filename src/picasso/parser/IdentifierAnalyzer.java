826a3fd066acbcc49e05444f65fc5f4f6bc4fce8
stockm23 <97920145+stockm23@users.noreply.github.com>
2022-12-08 17:44:20 -0500

    Revert "display error message"

--- src/picasso/parser/IdentifierAnalyzer.java
+++ src/picasso/parser/IdentifierAnalyzer.java
@@ -35,8 +35,6 @@ public class IdentifierAnalyzer implements SemanticAnalyzerInterface {
 			return mapped;
 		}
 
-		
-		
 		// TODO : What should we do if we don't recognize the identifier?
 		// Is that an error? Or, could there a valid reason?
 		return null;
