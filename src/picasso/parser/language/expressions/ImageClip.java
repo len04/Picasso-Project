eb7b42c0596f30f6b1d5e158bb7b19ca8562a249
petrailic <ilicp24@mail.wlu.edu>
2022-12-07 23:29:54 -0500

    updated ImageClip class and evaluator (dropping refactoring attempt for now) and fixing analyzer

--- src/picasso/parser/language/expressions/ImageClip.java
+++ src/picasso/parser/language/expressions/ImageClip.java
@@ -1,19 +1,18 @@
 package picasso.parser.language.expressions;
 
 import picasso.parser.language.ExpressionTreeNode;
-import picasso.parser.language.expressions.Clamp;
-import java.awt.image.*;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 
 /**
  * Represents the ImageClip function in the Picasso language.
  * 
  * @author Petra Ilic
  */
-public class ImageClip extends MultiArgFunction {
+public class ImageClip extends ExpressionTreeNode { 
+	
+	private Image image;
+	private ExpressionTreeNode x_param;
+	private ExpressionTreeNode y_param;
 
 	/**
 	 * Create an ImageClip expression that takes three parameters
@@ -22,32 +21,25 @@ public class ImageClip extends MultiArgFunction {
 	 * @param param2 the x coordinate expression
 	 * @param param3 the y coordinate expression
 	 */
-	public ImageClip(ExpressionTreeNode param1, ExpressionTreeNode param2, ExpressionTreeNode param3) {
-		super(param1, param2, param3);
+	public ImageClip(Image image, ExpressionTreeNode x_param, ExpressionTreeNode y_param) {
+		this.image = image;
+		this.x_param = x_param;
+		this.y_param = y_param;
 	}
 	
 	
-	
+	/**
+	 * Evaluates this image at the given x,y point by getting the color at the 
+	 * evaluated and clamped coordinated of the of the inputed image
+	 * 
+	 * @returns color at evaluated coordinates of inputed image
+	 */
 	@Override
 	public RGBColor evaluate(double x, double y) { 
-		double new_x = Clamp.clamp(param2.evaluate(x, y).getRed());
-		double new_y = Clamp.clamp(param3.evaluate(x, y).getRed());
-		
-		BufferedImage img = null;
-		String fileName = "vortex.jpg";
-		
-		// TODO: something is faulty with this - path?
-		try {
-			img = ImageIO.read(new File("vortex.jpg"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-//		int clr = img.getRGB(new_x, new_y);
-//      int red =   (clr & 0x00ff0000) >> 16;
-//      int green = (clr & 0x0000ff00) >> 8;
-//      int blue =   clr & 0x000000ff;
-		
-		return new RGBColor(1,-1,1);
+		double new_x = Clamp.clamp(x_param.evaluate(x, y).getRed());
+		double new_y = Clamp.clamp(y_param.evaluate(x, y).getRed());
+				
+		return image.evaluate(new_x, new_y);
 	}
 
 }
