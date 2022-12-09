02905142bc47d468062a8e39c262102c3edcdf9a
stockm23 <97920145+stockm23@users.noreply.github.com>
2022-12-09 02:13:14 -0500

    Update Frame.java

--- src/picasso/view/Frame.java
+++ src/picasso/view/Frame.java
@@ -2,8 +2,11 @@ package picasso.view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
+
 
 import picasso.model.Pixmap;
 import picasso.util.ThreadedCommand;
@@ -19,6 +22,13 @@ import picasso.view.commands.*;
 public class Frame extends JFrame {
 	private Canvas canvas;
 	private static JTextField inputField;	
+	public static JTextField errorField;
+	
+
+	
+	public static void setErrorField(String s) {
+		errorField.setText(s);
+	}
 	
 	public Frame(Dimension size) {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -33,25 +43,38 @@ public class Frame extends JFrame {
 		
 		
 		JLabel inputLabel = new JLabel ("Enter expression: ");
-		inputField = new JTextField(10);
-		// JButton evaluate = new JButton("Evaluate");		
+		errorField = new JTextField (25);
+		inputField = new JTextField(20);
+		JButton evaluate = new JButton("Evaluate");	
 		
 		// add commands to test here
 		ButtonPanel commands = new ButtonPanel(canvas);
 		commands.add("Open", new Reader());
 		commands.add(inputLabel);
 		commands.add(inputField);
-		commands.add("Evaluate", new ThreadedCommand<Pixmap>(canvas, new Evaluator()));
+		commands.add(evaluate);
 		commands.add("Save", new Writer());
+		commands.add(errorField);
 		
+
+		evaluate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ThreadedCommand<Pixmap> action = new ThreadedCommand<Pixmap>(canvas, new Evaluator());									
+				action.execute(canvas.getPixmap());
+				errorField.setText("");
+				canvas.refresh();
+				// inputField.setText("");
+				}
+		});
 		
 		// add our container to Frame and show it
 		getContentPane().add(canvas, BorderLayout.CENTER);
 		getContentPane().add(commands, BorderLayout.NORTH);
 		pack();
+	
 	}
 
-	public static JTextField getInput() {
-		return inputField;
+	public static String getInput() {
+		return inputField.getText();
 	}
 }
