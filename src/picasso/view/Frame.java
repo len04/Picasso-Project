6f5823dc1f344731e221503d287bac043390ca61
stockm23 <97920145+stockm23@users.noreply.github.com>
2022-12-09 02:46:40 -0500

    fixed frame

--- src/picasso/view/Frame.java
+++ src/picasso/view/Frame.java
@@ -2,11 +2,8 @@ package picasso.view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.*;
-
 
 import picasso.model.Pixmap;
 import picasso.util.ThreadedCommand;
@@ -23,9 +20,7 @@ public class Frame extends JFrame {
 	private Canvas canvas;
 	private static JTextField inputField;	
 	public static JTextField errorField;
-	
 
-	
 	public static void setErrorField(String s) {
 		errorField.setText(s);
 	}
@@ -41,40 +36,36 @@ public class Frame extends JFrame {
 		JTextField newTitle = new JTextField("Champions");		
 		this.setTitle(newTitle.getText());		
 		
-		
+		JLabel errorLabel = new JLabel("Error: ");
 		JLabel inputLabel = new JLabel ("Enter expression: ");
 		errorField = new JTextField (25);
-		inputField = new JTextField(20);
-		JButton evaluate = new JButton("Evaluate");	
+		inputField = new JTextField(20);	
+		// JButton evaluate = new JButton("Evaluate");		
 		
 		// add commands to test here
 		ButtonPanel commands = new ButtonPanel(canvas);
 		commands.add("Open", new Reader());
 		commands.add(inputLabel);
 		commands.add(inputField);
-		commands.add(evaluate);
+		commands.add("Evaluate", new ThreadedCommand<Pixmap>(canvas, new Evaluator()));
 		commands.add("Save", new Writer());
+		commands.add(errorLabel);
 		commands.add(errorField);
+		setErrorField("");
 		
-
-		evaluate.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				ThreadedCommand<Pixmap> action = new ThreadedCommand<Pixmap>(canvas, new Evaluator());									
-				action.execute(canvas.getPixmap());
-				errorField.setText("");
-				canvas.refresh();
-				// inputField.setText("");
-				}
-		});
 		
 		// add our container to Frame and show it
 		getContentPane().add(canvas, BorderLayout.CENTER);
 		getContentPane().add(commands, BorderLayout.NORTH);
 		pack();
-	
 	}
 
-	public static String getInput() {
-		return inputField.getText();
+	public static JTextField getInput() {
+		return inputField;
+	}
+
+	public static JTextField getErrorField() {
+		return errorField;
 	}
 }
+
