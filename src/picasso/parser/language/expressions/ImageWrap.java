182c2055a983df068b87c42cd91f31d80a43a173
petrailic <ilicp24@mail.wlu.edu>
2022-12-12 16:05:36 -0500

    implementing imageWrap and imageClip clean up

--- src/picasso/parser/language/expressions/ImageWrap.java
+++ src/picasso/parser/language/expressions/ImageWrap.java
@@ -4,24 +4,24 @@ import picasso.parser.language.ExpressionTreeNode;
 
 
 /**
- * Represents the ImageClip function in the Picasso language.
+ * Represents the ImageWrap function in the Picasso language.
  * 
  * @author Petra Ilic
  */
-public class ImageClip extends ExpressionTreeNode { 
+public class ImageWrap extends ExpressionTreeNode { 
 	
 	private Image image;
 	private ExpressionTreeNode x_param;
 	private ExpressionTreeNode y_param;
 
 	/**
-	 * Create an ImageClip expression that takes three parameters
+	 * Create an ImageWrap expression that takes three parameters
 	 * 
 	 * @param param1 string name of the image
 	 * @param param2 the x coordinate expression
 	 * @param param3 the y coordinate expression
 	 */
-	public ImageClip(Image image, ExpressionTreeNode x_param, ExpressionTreeNode y_param) {
+	public ImageWrap(Image image, ExpressionTreeNode x_param, ExpressionTreeNode y_param) {
 		this.image = image;
 		this.x_param = x_param;
 		this.y_param = y_param;
@@ -30,14 +30,14 @@ public class ImageClip extends ExpressionTreeNode {
 	
 	/**
 	 * Evaluates this image at the given x,y point by getting the color at the 
-	 * evaluated and clamped coordinates of the of the inputed image
+	 * evaluated and wrapped coordinates of the of the inputed image
 	 * 
 	 * @returns color at evaluated coordinates of inputed image
 	 */
 	@Override
 	public RGBColor evaluate(double x, double y) { 
-		double new_x = Clamp.clamp(x_param.evaluate(x, y).getRed());
-		double new_y = Clamp.clamp(y_param.evaluate(x, y).getRed());
+		double new_x = Wrap.wrap(x_param.evaluate(x, y).getRed());
+		double new_y = Wrap.wrap(y_param.evaluate(x, y).getRed());
 				
 		return image.evaluate(new_x, new_y);
 	}
