ea3f48ddf05846799353b6ba3c65c5a2abfc4424
lbngoc1804 <lbngoc1804@gmail.com>
2022-12-13 18:55:31 -0500

    Perlin Color function, no tests yet

---
+++ src/picasso/parser/PerlinColorAnalyzer.java
@@ -0,0 +1,29 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.PerlinColor;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing the Perlin Color function.
+ * 
+ * @author Ngoc Le
+ * 
+ */
+public class PerlinColorAnalyzer implements SemanticAnalyzerInterface {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); // Remove the times token
+		// the parameters are the next tokens on the stack.
+		ExpressionTreeNode param1 = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		ExpressionTreeNode param2 = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+				
+		return new PerlinColor(param1, param2);
+	}
+
+}
