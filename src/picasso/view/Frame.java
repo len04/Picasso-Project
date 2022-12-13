cfc6f82fa0511a9dea522e1cfc82ac613a5ae87b
FekryMostafa <mostafaf25@mail.wlu.edu>
2022-12-13 14:31:22 -0500

    Working Variable display with thumbnail

--- src/picasso/view/Frame.java
+++ src/picasso/view/Frame.java
@@ -1,8 +1,6 @@
 package picasso.view;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Image;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -33,72 +31,87 @@ public class Frame extends JFrame {
 	public static void setErrorField(String s) {
 		errorField.setText(s);
 	}
-	
+
 	public Frame(Dimension size) {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		// create GUI components
 		canvas = new Canvas(this);
 		canvas.setSize(size);
-		
+
 		// changing title of window
 		JTextField newTitle = new JTextField("Champions");		
 		this.setTitle(newTitle.getText());		
-		
+
 		JLabel errorLabel = new JLabel("Error: ");
 		JLabel inputLabel = new JLabel ("Enter expression: ");
 		errorField = new JTextField (10);
 		inputField = new JTextField(10);	
 		errorField.setEditable(false);
-		
-        myPane = new JPanel();
-        myPane.setSize(500, 200);
-        myPane.setLayout(new BoxLayout(myPane, BoxLayout.Y_AXIS));  
-        myPane.setVisible(true);
-        
+
+		myPane = new JPanel();
+		myPane.setSize(500, 200);
+		myPane.setLayout(new BoxLayout(myPane, BoxLayout.Y_AXIS));  
+		myPane.setVisible(true);
+
 		// add commands to test here
 		ButtonPanel commands = new ButtonPanel(canvas);
 		commands.add("Open", new Reader());
 		commands.add(inputLabel);
 		commands.add(inputField);
 		Evaluate = commands.add("Evaluate", new ThreadedCommand<Pixmap>(canvas, new Evaluator()));
-		
+
 		commands.add("Save", new Writer());
 		commands.add(errorLabel);
 		commands.add(errorField);
 		setErrorField("");
-		
+
 		// add our container to Frame and show it
 		getContentPane().add(canvas, BorderLayout.CENTER);
 		getContentPane().add(commands, BorderLayout.NORTH);
 		getContentPane().add(myPane, BorderLayout.EAST);
 		pack();
 	}
-	
+
 	public static void Adder() {
 		String text = inputField.getText();
 		int index = text.lastIndexOf("=");
 		String exp = text.substring( index + 1);
-		String name = text.substring(0, index);		
+		String name = text.substring(0, index);
+//		if (VarToExp.containsKey(name)) {
+//			removeButton(name + "=" + exp);
+//		}
 		VarToExp.put(name, exp);
-		myPane.removeAll();
-		for (String key : VarToExp.keySet()) { 
-		    String value = VarToExp.get(key);
-	        JButton button = new JButton(key + " = " + value);
-	        myPane.add(button);
-	        BufferedImage img = canvas.getPixmap().getMyImage();
-	        Image newimg = img.getScaledInstance( 25, 25,  java.awt.Image.SCALE_SMOOTH) ;  	        
-	        button.setIcon(new ImageIcon(newimg));
-			button.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					inputField.setText(button.getText());
-					Evaluate.doClick();
-					}
-				});
-		}
+		JButton button = new JButton(name + "=" + exp);	
+		myPane.add(button);
+		BufferedImage img = canvas.getPixmap().getMyImage();
+		Image newimg = img.getScaledInstance( 25, 25,  java.awt.Image.SCALE_SMOOTH) ;  	        
+		button.setIcon(new ImageIcon(newimg));
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				inputField.setText(button.getText());
+				Evaluate.doClick();
+			}
+		});
 		myPane.revalidate();
 		myPane.repaint();
 	}
+
+	private static void removeButton(String string) {
+		System.out.println("test");		
+		Component[] components = myPane.getComponents();
+		for (Component component : components) {
+			 if (component instanceof JButton) {
+			        JButton button = (JButton) component;	
+			String text = ((JButton) component).getText();
+			System.out.println(text);
+			if (text.equals(string)) {
+				myPane.remove(component);
+			}
+		}
+	}
+}
+
 
 	public static JTextField getInput() {
 		return inputField;
