2db368d88eaab7f3d1f2c8193eb49afd94b8114d
lbngoc1804 <lbngoc1804@gmail.com>
2022-12-14 15:39:54 -0500

    some ParsedExpTreeTests

--- src/picasso/parser/PerlinColorAnalyzer.java
+++ src/picasso/parser/PerlinColorAnalyzer.java
@@ -16,14 +16,15 @@ public class PerlinColorAnalyzer implements SemanticAnalyzerInterface {
 
 	@Override
 	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
-		tokens.pop(); // Remove the times token
+		tokens.pop(); // Remove the Perlin Color token
+		
 		// the parameters are the next tokens on the stack.
 		ExpressionTreeNode param1 = SemanticAnalyzer.getInstance().generateExpressionTree(
 				tokens);
 		ExpressionTreeNode param2 = SemanticAnalyzer.getInstance().generateExpressionTree(
 				tokens);
 				
-		return new PerlinColor(param1, param2);
+		return new PerlinColor(param2, param1);
 	}
 
 }
